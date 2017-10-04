5eab71d59c40af1fb5d7e089772a77274949bfe8
Jire <thomasgnappo@gmail.com>
2017-10-04 12:31:56 -0400

    Travis & license

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Layer 7 Shield
 _A primitive but effective shield against layer 7 DoS attacks for NGINX on Windows_
+
+[![Build Status](https://travis-ci.org/Jire/layer7shield.svg?branch=master)](https://travis-ci.org/Jire/layer7shield)
+[![License](https://img.shields.io/github/license/Jire/layer7shield.svg)](https://github.com/Jire/layer7shield/blob/master/LICENSE.txt)
--- src/main/kotlin/org/jire/layer7shield/Layer7Shield.kt
+++ src/main/kotlin/org/jire/layer7shield/Layer7Shield.kt
@@ -15,25 +15,25 @@ const val INI_FILE = "layer7shield.ini"
 fun main(args: Array<String>) {
 	println("Layer7Shield [${ManagementFactory.getRuntimeMXBean().name}]")
 	
-	val ini = IniPreferences(Ini(File(INI_FILE)))
-	
-	ini.node("layer7shield")?.run {
-		duration = get("duration", duration.toString()).toLong()
-		requests_per_duration = get("requests_per_duration", requests_per_duration.toString()).toInt()
-		
-		print_logs = get("print_logs", print_logs.toString()).toBoolean()
-		print_blocks = get("print_blocks", print_blocks.toString()).toBoolean()
-	}
-	ini.node("log file")?.run {
-		path = get("path", path)
-		parse_duration = get("parse_duration", parse_duration.toString()).toLong()
-		parse_buffer_size = get("parse_buffer_size", parse_buffer_size.toString()).toInt()
-	}
-	ini.node("firewall")?.run {
-		rule_name = get("rule_name", rule_name)
-		
-		add_rule_format = get("add_rule_format", add_rule_format)
-		delete_rule_format = get("delete_rule_format", delete_rule_format)
+	IniPreferences(Ini(File(INI_FILE))).run {
+		node("layer7shield")?.run {
+			duration = get("duration", duration.toString()).toLong()
+			requests_per_duration = get("requests_per_duration", requests_per_duration.toString()).toInt()
+			
+			print_logs = get("print_logs", print_logs.toString()).toBoolean()
+			print_blocks = get("print_blocks", print_blocks.toString()).toBoolean()
+		}
+		node("log file")?.run {
+			path = get("path", path)
+			parse_duration = get("parse_duration", parse_duration.toString()).toLong()
+			parse_buffer_size = get("parse_buffer_size", parse_buffer_size.toString()).toInt()
+		}
+		node("firewall")?.run {
+			rule_name = get("rule_name", rule_name)
+			
+			add_rule_format = get("add_rule_format", add_rule_format)
+			delete_rule_format = get("delete_rule_format", delete_rule_format)
+		}
 	}
 	
 	val logFile = File(path)
