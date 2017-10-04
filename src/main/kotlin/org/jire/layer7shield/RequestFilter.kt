1355684cd0b8afc5c0ac27839e6313b3bdcdd486
Jire <thomasgnappo@gmail.com>
2017-10-04 13:28:06 -0400

    Changed filter to only take into consideration dynamic workloads

--- src/main/kotlin/org/jire/layer7shield/RequestFilter.kt
+++ src/main/kotlin/org/jire/layer7shield/RequestFilter.kt
@@ -5,11 +5,13 @@ object RequestFilter {
 	private val ipToRequests = HashMap<String, Int>()
 	
 	fun filter(ip: String, request: String) {
-		if (log_requests) println("$ip $request")
-		
-		val requests = ipToRequests.getOrDefault(ip, 0)
-		if (requests == requests_per_duration) Firewall.ban(ip)
-		else if (requests < requests_per_duration) ipToRequests.put(ip, requests + 1)
+		if (!request.contains('.') || request.contains(".php")) { // only for dynamic workloads
+			if (log_requests) println("$ip $request")
+			
+			val requests = ipToRequests.getOrDefault(ip, 0)
+			if (requests == requests_per_duration) Firewall.ban(ip)
+			else if (requests < requests_per_duration) ipToRequests.put(ip, requests + 1)
+		}
 	}
 	
 	fun clear() {
